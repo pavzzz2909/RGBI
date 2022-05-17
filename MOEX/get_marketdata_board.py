854ff645a9138514d2b76e20c3f13f1769dc1792
Pavel <pavzzz@yandex.ru>
2022-05-18 00:42:25 +0600

    Добавление инструментов

--- MOEX/get_marketdata_board.py
+++ MOEX/get_marketdata_board.py
@@ -27,11 +27,29 @@ def get_marketdata_board(dict_columns, dict_RGBI, dict_all, board='TQOB'):
     for key in response.keys():
         for row in response[key]['data']:
             dict_all[number_index] = {}
+            dict_all[number_index]['secid'] = row[0]
+            number_index +=1
+
+
+    for key in response.keys():
+        for row in response[key]['data']:
+            for secid in dict_all.keys():
+                if row[0] == dict_all[secid]['secid']:
+                    for i in range(0,len(row)):
+                        if response[key]['columns'][i] in dict_columns.keys():
+                            name = dict_columns[response[key]['columns'][i]]
+                            itog = row[i]
+                            dict_all[secid][name] = itog
+
+    '''
+    for key in response.keys():
+        for row in response[key]['data']:
+            dict_all[number_index] = {}
             for i in range(0,len(row)):
                 if response[key]['columns'][i] in dict_columns.keys():
                     name = dict_columns[response[key]['columns'][i]]
                     itog = row[i]
                     dict_all[number_index][name] = itog
             number_index +=1
-
+    '''
     return dict_RGBI, dict_all
